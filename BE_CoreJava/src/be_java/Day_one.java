4989d98dd1b2c3e572743c90cc414af7051fe02b
Lenovo <Lenovo@DESKTOP-IOTOQ92>
2023-07-20 22:49:57 +0530

    First commit

---
+++ BE_CoreJava/src/be_java/Day_one.java
@@ -0,0 +1,10 @@
+package be_java;
+
+public class Day_one {
+
+	public static void main(String[] args) {
+		System.out.println("*****This is my new project*****");
+
+	}
+
+}
---
+++ BE_CoreJava/src/module-info.java
@@ -0,0 +1,2 @@
+module BE_CoreJava {
+}
